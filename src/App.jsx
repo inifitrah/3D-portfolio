37770e7ff42106ae6340e24ea717330d50a3b7d1
fitrah <mftrhr@gmail.com>
2024-09-21 19:16:57 +0800

    add: hhero content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import TailwindSizeIndicator from "./components/TailwindSizeIndicator";
 import { Hero } from "./section/Hero";
 import { Navbar } from "./section/Navbar";
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
+      {import.meta.env.MODE === "development" && <TailwindSizeIndicator />}
       <Navbar />
       <Hero />
     </main>
--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
-export const Hero = () => {
-  return <div>Hero</div>;
-};
+export const Hero = () => (
+  <section className="text-white min-h-screen flex flex-col relative" id="home">
+    <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+      <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+        Hi, I am Fitrah <span className="waving-hand">👋</span>
+      </p>
+      <p className="hero_tag text-gray_gradient">Software Engineer</p>
+    </div>
+
+    <div className="flex-grow">
+      <Canvas>
+        <ambientLight intensity={1} />
+        <directionalLight position={[0, 10, 5]} intensity={1} />
+        <mesh scale={[8, 2, 2]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  </section>
+);
